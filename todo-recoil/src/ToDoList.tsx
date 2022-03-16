de6a5cf5dd409102c7084589d15710b3ceff0a09
ezdar2743@naver.com <ezdar2743@gmail.com>
2022-03-16 19:42:50 +0900

    init for toDoList

--- todo-recoil/src/ToDoList.tsx
+++ todo-recoil/src/ToDoList.tsx
@@ -1,60 +1,29 @@
-import React, { useState } from 'react';
-import {useForm} from "react-hook-form";
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
-interface IForm{
-    Email: string;
-    password: string;
-    password1: string;
-
+interface IToDo{
+    toDo: string
 }
-const ToDoList = () => {
-    const {register, handleSubmit,formState:{errors},setError}= useForm<IForm>({
-        defaultValues: { 
-            Email: "@naver.com"
-        }
-    });
-    const onValid = (data:IForm)=>{
-       if(data.password !==data.password1){
-           setError("password1",{message:"now same"},
-           {shouldFocus:true})
-       }
-    }
-    
-    return(
-        <>
-            <form onSubmit={handleSubmit(onValid)}>
-                <input 
-                placeholder='Input your Email'
-                {...register("Email",
-                {required:"Email is required",
-                validate: {noEz:(value)=>value.includes("ez")? "can not use ez":true},
-                pattern:{
-                    value:/^[A-Za-z0-9._%+-]+@naver.com$/,
-                    message: "Only naver.com emails allowed"},
-                minLength:{
-                    value:5,
-                    message: "over 5"},
-                    
-                })}/>
-                <span>
-                    {errors?.Email?.message}
-                </span>
 
+
+const ToDoList = () => {
+    const {register, handleSubmit,setValue}= useForm<IToDo>();
+    const onSubmit=(data:IToDo)=>{
+        console.log(data.toDo)
+        setValue("toDo","")    
+    }
+    return (
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <input 
-                placeholder='Input your password'
-                {...register("password",{required:"Id is required"})}/>
-                <span>{errors?.password?.message}</span>
-                <input 
-                placeholder='Input your password'
-                {...register("password1",{required:true})}
+                placeholder='Input your to do'
+                {...register("toDo",{required:"Please Input your to do"} )}
                 />
-                <span>{errors?.password1?.message}</span>
-            
                 <button>Add</button>
             </form>
+            
         </>
-    )
-    
+    );
 };
 
 export default ToDoList;
