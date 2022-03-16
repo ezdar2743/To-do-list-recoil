8ec4380dea4eb34f787d57c7dc13bbbb43860bd7
ezdar2743@naver.com <ezdar2743@gmail.com>
2022-03-16 16:59:27 +0900

    default value and handle error, should focus

--- todo-recoil/src/ToDoList.tsx
+++ todo-recoil/src/ToDoList.tsx
@@ -1,32 +1,53 @@
 import React, { useState } from 'react';
 import {useForm} from "react-hook-form";
 
+interface IForm{
+    Email: string;
+    password: string;
+    password1: string;
 
+}
 const ToDoList = () => {
-    const {register, handleSubmit,formState}= useForm();
-    const onValid = (data:any)=>{
-        console.log(data)
+    const {register, handleSubmit,formState:{errors},setError}= useForm<IForm>({
+        defaultValues: { 
+            Email: "@naver.com"
+        }
+    });
+    const onValid = (data:IForm)=>{
+       if(data.password !==data.password1){
+           setError("password1",{message:"now same"},
+           {shouldFocus:true})
+       }
     }
-    console.log(formState.errors)
+    
     return(
         <>
             <form onSubmit={handleSubmit(onValid)}>
                 <input 
                 placeholder='Input your Email'
-                {...register("Email",{required:"Email is required",minLength:{
+                {...register("Email",
+                {required:"Email is required",
+                pattern:{
+                    value:/^[A-Za-z0-9._%+-]+@naver.com$/,
+                    message: "Only naver.com emails allowed"},
+                minLength:{
                     value:5,
                     message: "over 5"
-                }})}
-                ></input>
-                <input 
-                placeholder='Input your Id'
-                {...register("FirstName",{required:"Id is required"})}
-                ></input>
+                }})}/>
+                <span>
+                    {errors?.Email?.message}
+                </span>
+
                 <input 
                 placeholder='Input your password'
-                {...register("LastName",{required:true})}
-                ></input>
-               
+                {...register("password",{required:"Id is required",minLength:{value:5, message:"over 5"}})}
+                />
+                <span>{errors?.password?.message}</span>
+                <input 
+                placeholder='Input your password'
+                {...register("password1",{required:true})}
+                />
+                <span>{errors?.password1?.message}</span>
             
                 <button>Add</button>
             </form>
